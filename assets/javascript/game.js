b1b3fab751e80affff541f9589493ecb812cd06a
QuantumT95 <tran.charlie@ymail.com>
2017-06-29 17:12:14 -0500

    made the game but still need to add win, loss, and guesses onto the page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,20 @@
-var guess;
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
+// Creating an empty array for alphabet and variables //
+      var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+      var win = 0;
+      var loss = 0;
+      var guesses = 3;
 
+      // math random //
+      var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
+      console.log(computerChoice);
 
+      while (Attempts != computerChoice) {
+        var Attempts = prompt("You have 10 guesses. Guess between 'a' to 'z'.");
+        guesses -= 1; 
 
-
-
-
-for (var i=0; i<9; i++){
-	console.log(i);
-}
+        if (guesses = 0){
+          document.write("You have lost!");
+          break;
+        }
+      }
