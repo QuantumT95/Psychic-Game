d4727b2e87bc591956ca7eccef23af5923558922
QuantumT95 <tran.charlie@ymail.com>
2017-06-29 22:53:21 -0500

    changed some javascript, tried to add reset function and documentation onto the page... keyword tried

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,46 @@
       var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
       var win = 0;
       var loss = 0;
-      var guesses = 3;
+      var guesses = 10;
+      var guessedLetters =[];
+      var newLetter = null;
 
       // math random //
       var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-
       console.log(computerChoice);
 
-      while (Attempts != computerChoice) {
-        var Attempts = prompt("You have 10 guesses. Guess between 'a' to 'z'.");
-        guesses -= 1; 
 
-        if (guesses = 0){
-          document.write("You have lost!");
-          break;
+      // reset //
+      var reset = function() {
+        guesses = 10;
+        guessedLetters = [];
+        changeLettertoGuess();
+      }
+
+      // change computer Choice when reset happens //
+      var changeLettertoGuess = function() {
+        this.newLetter = this.alphabet[Math.floor(Math.random() * alphabet.length)];
+      }
+
+      // loop //
+      while (Attempts != computerChoice) {
+        var Attempts = prompt("You have 10 guesses. Guess between 'a' to 'z'.")
+
+        if (Attempts == computerChoice){
+          document.write("You got it buddy!");
+          document.write("<p> The letter was " + computerChoice + "</p>");
+          document.write("<p> Amount of guesses = " + guesses + "</p>");
+          win += 1;
+          reset();
+        } 
+
+        if (Attempts != computerChoice){
+          guesses -= 1;
+        }
+
+        if (guesses === 0){
+          document.write("YOU LOST!");
+          loss += 1;
+          reset(); 
         }
       }
