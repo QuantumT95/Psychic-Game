503ad155634d5ddd6d909a44e1c7586fdebb7a3d
QuantumT95 <tran.charlie@ymail.com>
2017-07-03 10:28:47 -0500

    found a way to not prompt, still need reset and way to add already used guesses onto screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,46 @@
-// Creating an empty array for alphabet and variables //
+      // Creating an empty array for alphabet and variables //
       var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
       var win = 0;
       var loss = 0;
       var guesses = 10;
-      var guessedLetters =[];
       var newLetter = null;
 
       // math random //
       var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
       console.log(computerChoice);
 
-      var updateGuessesSoFar = function() {
-      // Grabs, declares, and updates the player's past guesses
-      document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
-      };
+      // loop //
 
+      document.onkeyup = function(event) {
+        guesses -= 1;
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
       // reset //
-      var reset = function() {
-        guesses = 10;
-        guessedLetters = [];
+      var reset = function(){
+        changeLettertoGuess();
+
       }
 
       // change computer Choice when reset happens //
       var changeLettertoGuess = function() {
-        this.newLetter = this.alphabet[Math.floor(Math.random() * alphabet.length)];
+      alphabet[Math.floor(Math.random() * alphabet.length)];
       }
 
-      // loop //
-      while (Attempts != computerChoice) {
-        var Attempts = prompt("You have 10 guesses. Guess between 'a' to 'z'.")
-
-        if (Attempts == computerChoice){
-          document.write("You got it buddy!");
-          document.write("<p> The letter was " + computerChoice + "</p>");
-          document.write("<p> Amount of guesses left = " + guesses + "</p>");
-          win += 1;
-          reset;
+      if (userGuess == computerChoice){
+        document.write("You got it buddy!");
+        document.write("<p> The letter was " + computerChoice + "</p>");
+        document.write("<p> Amount of guesses left = " + guesses + "</p>");
+        win += 1;
+        reset();
         } 
 
-        if (Attempts != computerChoice){
-          guesses -= 1;
-        }
-
-        if (guesses === 0){
-          document.write("YOU LOST!");
-          loss += 1;
-          reset; 
+      if (guesses === 0){
+        document.write("YOU LOST!");
+        loss += 1;
+        reset();
         }
       }
-
+      // wins and losses will show on screen //
       document.getElementById("win").innerHTML = "Wins: " + win;
       document.getElementById("loss").innerHTML = "Losses: " + loss;
       
